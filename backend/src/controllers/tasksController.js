1b9524fffb1ff537bfe9d106fee261e068bfac9d
Arley Souza <darkschneider.arley@gmail.com>
2023-09-29 15:06:12 -0300

    Atualização final do backend, com integração do put, delete, validação dos middlewares

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,13 +1,34 @@
 const tasksModel = require('../models/tasksModel');
 
-const getAll = async (request, response) => {
-
-  
+const getAll = async (_request, response) => {
   const tasks = await tasksModel.getAll();
-
   return response.status(200).json(tasks);
 };
 
+const createTask = async (request, response) => {
+  const createTask = await tasksModel.createTask(request.body);
+  return response.status(201).json(createTask);
+};
+
+const delTask = async (request, response) => {
+  const { id } = request.params;
+
+  await tasksModel.delTask(id);
+  return response.status(204).json();
+
+};
+
+const updateTask = async (request, response) => {
+  const { id } = request.params;
+
+  await tasksModel.updateTask(id, request.body);
+  return response.status(204).json();
+
+};
+
 module.exports = {
-  getAll
+  getAll,
+  createTask,
+  delTask,
+  updateTask,
 };
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,8 +1,19 @@
 const express = require('express');
-const tasksController =  require('./controllers/tasksController');
 
 const router = express.Router();
 
+const tasksController =  require('./controllers/tasksController');
+const tasksMiddleware = require('./middlewares/tasksMiddlewares');
+
+
+
 router.get('/tasks', tasksController.getAll);
+router.post('/tasks', tasksMiddleware.validateBody, tasksController.createTask);
+router.delete('/tasks/:id', tasksController.delTask);
+router.put('/tasks/:id',
+  tasksMiddleware.validateBody,
+  tasksMiddleware.validateFieldStatus,
+  tasksController.updateTask
+);
 
 module.exports = router;
